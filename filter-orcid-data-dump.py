3f9fd6726a06d6ecf5cc0570d26f8f0b21a2819e
Jan Dvorak <jdvorak001@users.noreply.github.com>
2019-10-05 11:30:16 +0200

    stop using asyncio

--- filter-orcid-data-dump.py
+++ filter-orcid-data-dump.py
@@ -4,7 +4,6 @@ import re
 import threading
 import queue
 import subprocess
-import asyncio
 import os
 import sys
 from lxml import etree
@@ -14,9 +13,6 @@ import time
 dump_file = "data/sample/ORCID-public-profiles-2018-API-2.0_xml-sample.tar.gz"
 num_worker_threads = 3
 queue_length = 200
-
-if sys.platform == "win32":
-    asyncio.set_event_loop_policy( asyncio.WindowsProactorEventLoopPolicy() )
 
 # expects a match() function to be defined: 
 #   given a xml root element, it returns True when the profile matches your condition
@@ -43,29 +39,23 @@ def worker():
             time.sleep( 0.01 )
 
 re1 = re.compile( "^x\\s+" )
-
-async def process( dump_file ):
-    # Create the "tar xzvf ${file}" subprocess; redirect the standard output into a pipe
-    untar_proc = await asyncio.create_subprocess_exec( "tar", "xzvf", dump_file, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE )
-    untar_list_stream = untar_proc.stderr if sys.platform == "darwin" else untar_proc.stdout
-    while True:
-        data = await untar_list_stream.readline()
-        if not data: break
-        filename = data.decode( "ascii" ).rstrip()
-        filename = re1.sub( "", filename )  # for compatibility with macOS tar
-        if filename[-1] != "/":
-            q.put( filename )
     
-    # Wait for the subprocess exit
-    await untar_proc.wait()
-
 threads = []
 for i in range( num_worker_threads ):
     t = threading.Thread( target=worker )
     t.start()
     threads.append( t )
 
-asyncio.run( process( dump_file ) )
+# Create the "tar xzvf ${file}" subprocess; redirect the standard output into a pipe
+untar_proc = subprocess.Popen( [ "tar", "xzvf", dump_file ], bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
+untar_list_stream = untar_proc.stderr if sys.platform == "darwin" else untar_proc.stdout
+while True:
+    data = untar_list_stream.readline()
+    if not data: break
+    filename = data.decode( "ascii" ).rstrip()
+    filename = re1.sub( "", filename )  # for compatibility with macOS tar
+    if filename[-1] != "/":
+        q.put( filename )
 
 # block until all tasks are done
 closing = True
@@ -76,3 +66,5 @@ for i in range( num_worker_threads ):
     q.put( None )
 for t in threads:
     t.join()
+
+untar_proc.wait()
